stathissideris/ditaa

Option to render SVG images

src/org/stathissideris/ascii2image/core/ConversionOptions.java
@@ -106,6 +106,14 @@ public class ConversionOptions {
 			processingOptions.setCharacterEncoding(encoding);
 		}
 		
+		if (cmdLine.hasOption("svg")){
+			renderingOptions.setImageType(RenderingOptions.ImageType.SVG);
+		}
+
+		if (cmdLine.hasOption("svg-font-url")){
+			renderingOptions.setFontURL(cmdLine.getOptionValue("svg-font-url"));
+		}
+
 		ConfigurationParser configParser = new ConfigurationParser();
 		try {
 			for (Option curOption : cmdLine.getOptions()) {

yuzutech/ditaa-mini

(cherry picked from commit 091abcc)

src/main/java/org/stathissideris/ascii2image/core/ConversionOptions.java
@@ -105,5 +105,13 @@ public class ConversionOptions {
             new String(new byte[2], encoding);
             processingOptions.setCharacterEncoding(encoding);
         }
+
+        if (cmdLine.containsKey("svg")){
+            renderingOptions.setImageType(RenderingOptions.ImageType.SVG);
+        }
+
+        if (cmdLine.containsKey("svg-font-url")){
+            renderingOptions.setFontURL(cmdLine.get("svg-font-url"));
+        }
     }
 }